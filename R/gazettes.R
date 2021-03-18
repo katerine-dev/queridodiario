be79dc3e37055673975c1b3d23942170807f2c88
rcfeliz <ric.feliz@gmail.com>
2021-03-18 17:17:42 -0300

    Add dependencies + pequena descrição dos params + correção das coisas simples

--- R/gazettes.R
+++ R/gazettes.R
@@ -1,15 +1,15 @@
 #' get_gazettes
 #'Descrição da função
 #'parâmetros
-#'
-#'
+#' @param since data a partir da qual, em formato YYY-MM-DD
+#' @param until data até quando, em formato YYY-MM-DD
+#' @param keywords palavras-chave que serão pesquisadas nos diários oficiais
+#' @param territory_id número dos municípios, seguindo os códigos da tabela
+#' @param offset
+#' @param size
 #'
 #'return descrição do resultado
 #'export
-
-library(httr)
-
-BASE_API_URL <- "https://queridodiario.ok.org.br/api/"
 
 get_gazettes <- function(since = NULL,
                      until = NULL,
@@ -17,6 +17,7 @@ get_gazettes <- function(since = NULL,
                      territory_id = NULL,
                      offset = 0,
                      size = 10) {
+  BASE_API_URL <- "https://queridodiario.ok.org.br/api/"
   endpoint <- "gazettes/"
   if (!is.null(territory_id)) {
     endpoint <-  sprintf("gazettes/%s", territory_id)
@@ -29,14 +30,13 @@ get_gazettes <- function(since = NULL,
   if (!is.null(until)) {
     payload <- append(payload, sprintf("until=%s", until))
   }
-  if (!is.null(keywords))
-    for (i in keyword) {
-      payload <- append(payload, sprintf("keyword=%s", keywords))
-    }
+  if (!is.null(keywords)) {
+    payload <- append(payload, sprintf("keywords=%s", keywords))
+  }
   url_params <-  paste(payload, collapse = "&")
   api  <-paste(c(BASE_API_URL, endpoint, "?", url_params), collapse = "")
-  response <- GET(api)
-  http_status(response)
+  response <- httr::GET(api)
+  response$status
   responseParsed <- content(response, as="parsed")
   responseParsed
 }
